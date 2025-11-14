eea04ca2e643e89e37656fd16816bd9a6c55f774
Justin <jjhatch03@gmail.com>
2025-11-13 22:14:07 -0800

    OPENAIAIAIAIA

--- backend/analysis/AI.py
+++ backend/analysis/AI.py
@@ -5,18 +5,22 @@
 # functionality that ChatGPT would directly do.
 
 from openai import OpenAI, AuthenticationError # pip install openai
-from backend.utils.support import get_secret
+#from backend.utils.support import get_secret
+import os
 
 class AI:
     
-    def __init__(self) -> None:
-        
-        self.key = get_secret("OPENAI_KEY")
+    def __init__(self, model="gpt-4o") -> None:
+    
+        self.key = os.getenv("OPENAI_API_KEY")
         self.client = self.__set_client()
+        self.model = model
 
     def __set_client(self):
+        client = OpenAI()
         try:
-            return # TODO: we are accessing the client via OpenAI SDK
+           client = OpenAI()
+           return client
         except Exception as error:
             print(f"Error ocurred when intializing the client. Error: {error}")
     
@@ -30,27 +34,32 @@ class AI:
             return True
         return False
     
-    def request_query(self, prompt):
+    def request_query(self, prompt:str):
         
         if not self.client:
             return {"message": "There was an error getting OpenAI client started", "response": {}}
         if not prompt:
             return {"message": "Prompt set is invalid", "response":{}}
         try:
-            # TODO: Use our client to access a response given the prompt.
-            # validate response using our method
-            content = ...
+            reponse = self.client.responses.create(
+                model = self.model,
+                input = prompt
+            )
+            content = reponse.output_text
             return {"message": "success", "response" : content}
         
         except AuthenticationError:
             return {"message":"API key is incorrect. Authentication is invalid", "response":{}}
-        except Exception:
-            return {"message":"There was an error initializing the prompt.", "response":{}}
+        except Exception as e:
+            return {"message":"There was an error initializing the prompt.", "response":{e}}
 
         
         
     
             
         
-    
-    
+if __name__ == "__main__":
+    model = AI()
+    ticker = input("Enter Ticker: ")
+    payload = model.request_query(f"Give me a short summary on {ticker}, maybe include recent news, lastly what is market sentemin? respond in paragraph form, no bullet points or headers or fancy titles")
+    print(payload["response"])
