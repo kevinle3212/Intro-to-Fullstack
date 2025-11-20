65019b95f3dd3dca033b2135d56a2a7e7d776604
kylercreech <kylercreechjp@gmail.com>
2025-11-19 19:23:43 -0800

    Search page v1 added

---
+++ frontend/my-app/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full bg-gray-900 text-white p-4 text-center mt-auto">
+      © 2025 Oregon Software Consulting. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
