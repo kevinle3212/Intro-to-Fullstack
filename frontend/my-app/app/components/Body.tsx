1c01df96ef83e5c7d6d67036df5cbac8267ffb93
Kellen Milligan <kellenm@uoregon.edu>
2025-11-20 17:38:58 -0800

    Made a new rough draft for the homepage

--- frontend/my-app/app/components/Body.tsx
+++ frontend/my-app/app/components/Body.tsx
@@ -5,38 +5,93 @@ const Body = () => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-1 flex-col bg-white items-center">
-        <p className="mt-18 text-[45px] text-black">
-            OSC Finance
-        </p>
-        <p className="mt-5 text-[20px] text-black">
-            Track Current Stock Prices For All Major Stocks
-        </p>
-
-        <button 
-            onClick={() => router.push('')} 
-            className="h-15 w-80 rounded-md mt-5 bg-[#444444] text-white text-[18px] hover:bg-[#343232]">
-                Search Stocks Now
-        </button>
-
-        <div className="pl-5 pr-5 mt-10 border w-22 h-14 text-gray-500 text-center text-[18px]">
-            S&P 500
-        </div>
-
-        <div className="flex flex-col mb-20 mt-2 pt-10 border rounded-md w-270 h-95 text-gray-500 text-center
-                        items-center"> 
-            Tagline
-            <p className="mt-8 text-[42px] text-black font-bold">
-                $99
-            </p>
-            <div className="h-20 w-50 mt-8 text-black text-left pl-5">
-                Feature text goes here Feature text goes here Feature text goes here and more
+    <div className="flex flex-1 flex-col h-screen bg-white">
+        <div className="flex flex-1">
+            <div className="img-box h-[500px] w-[550px]">
+                <img src="stocks.gif" alt="Stocks Animation" className="w-full h-full object-cover">
+                </img>
             </div>
-        <button 
-            onClick={() => router.push('')} 
-            className="h-10 w-22 rounded-md mt-8 bg-[#444444] text-white text-[18px] hover:bg-[#343232]">
-                Button
-        </button>
+
+            <div className="flex flex-col flex-1 text-center border-black border-[1px] h-[500px]">
+                <p className="text-[45px] text-black h-[80px]">
+                    Top Stocks 
+                </p>
+                <div className="flex flex-col text-center border-black border-[1px] h-[500px] overflow-y-auto">
+                    <div className="h-[125px] justify-center items-center 
+                    flex border-black border-[1px] border-l-0 text-black">
+                        <p className="text-[26px]">
+                            Stock
+                        </p>
+                        <img src="stocks.gif" alt="Stocks Animation" className="w-60 h-20 ml-20 mr-20 hover:opacity-80 duration-200">
+                        </img>
+                        <p className="text-[26px]">
+                            Value
+                        </p>
+                    </div>
+
+                    <div className="h-[125px] justify-center items-center 
+                    flex border-black border-[1px] border-l-0 text-black">
+                        <p className="text-[26px]">
+                            Stock
+                        </p>
+                        <img src="stocks.gif" alt="Stocks Animation" className="w-60 h-20 ml-20 mr-20 hover:opacity-80 duration-200">
+                        </img>
+                        <p className="text-[26px]">
+                            Value
+                        </p>
+                    </div>
+
+                    <div className="h-[125px] justify-center items-center 
+                    flex border-black border-[1px] border-l-0 text-black">
+                        <p className="text-[26px]">
+                            Stock
+                        </p>
+                        <img src="stocks.gif" alt="Stocks Animation" className="w-60 h-20 ml-20 mr-20 hover:opacity-80 duration-200">
+                        </img>
+                        <p className="text-[26px]">
+                            Value
+                        </p>
+                    </div>
+
+                    <div className="h-[125px] justify-center items-center 
+                    flex border-black border-[1px] border-l-0 text-black">
+                        <p className="text-[26px]">
+                            Stock
+                        </p>
+                        <img src="stocks.gif" alt="Stocks Animation" className="w-60 h-20 ml-20 mr-20 hover:opacity-80 duration-200">
+                        </img>
+                        <p className="text-[26px]">
+                            Value
+                        </p>
+                    </div>
+
+                </div> 
+            </div>
+        </div>
+        <div className="flex h-[100px] w-[1300px] justify-center items-center">
+            <button className="h-full w-[190px] text-black text-[20px]
+            hover:bg-gray-200 border-r">
+                Day
+            </button>
+            <button className="h-full w-[190px] text-black text-[20px]
+            hover:bg-gray-200 border-r">
+                Week
+            </button>
+            <button className="h-full w-[190px] text-black text-[20px]
+            hover:bg-gray-200 border-r">
+                Month
+            </button>
+            <button className="h-full w-[190px] text-black text-[20px]
+            hover:bg-gray-200 border-r">
+                Year
+            </button>
+            <button className="h-full w-[190px] text-black text-[20px]
+            hover:bg-gray-200 border-r">
+                5 Years
+            </button>
+            <div className="flex h-full w-full justify-center items-center text-white text-[32px] bg-black hover:bg-gray-800">
+                    Search Stocks
+            </div>
         </div>
     </div>
   );
