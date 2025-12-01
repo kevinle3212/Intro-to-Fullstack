6283c5f38fdd591750191196c24c55a2d3dcc14b
Kellen Milligan <kellenm@uoregon.edu>
2025-12-01 11:49:43 -0800

    Restyled Home Page a little bit

--- frontend/my-app/app/components/Body.tsx
+++ frontend/my-app/app/components/Body.tsx
@@ -1,100 +1,67 @@
-'use client';
-import { useRouter } from 'next/navigation';
+"use client";
+import { useRouter } from "next/navigation";
 
-const Body = () => {
+export default function Body() {
   const router = useRouter();
 
   return (
-    <div className="flex flex-1 flex-col h-screen bg-white">
-        <div className="flex flex-1">
-            <div className="img-box h-[500px] w-[550px]">
-                <img src="stocks.gif" alt="Stocks Animation" className="w-full h-full object-cover">
-                </img>
-            </div>
-
-            <div className="flex flex-col flex-1 text-center border-black border-[1px] h-[500px]">
-                <p className="text-[45px] text-black h-[80px]">
-                    Top Stocks 
-                </p>
-                <div className="flex flex-col text-center border-black border-[1px] h-[500px] overflow-y-auto">
-                    <div className="h-[125px] justify-center items-center 
-                    flex border-black border-[1px] border-l-0 text-black">
-                        <p className="text-[26px]">
-                            Stock
-                        </p>
-                        <img src="stocks.gif" alt="Stocks Animation" className="w-60 h-20 ml-20 mr-20 hover:opacity-80 duration-200">
-                        </img>
-                        <p className="text-[26px]">
-                            Value
-                        </p>
-                    </div>
-
-                    <div className="h-[125px] justify-center items-center 
-                    flex border-black border-[1px] border-l-0 text-black">
-                        <p className="text-[26px]">
-                            Stock
-                        </p>
-                        <img src="stocks.gif" alt="Stocks Animation" className="w-60 h-20 ml-20 mr-20 hover:opacity-80 duration-200">
-                        </img>
-                        <p className="text-[26px]">
-                            Value
-                        </p>
-                    </div>
-
-                    <div className="h-[125px] justify-center items-center 
-                    flex border-black border-[1px] border-l-0 text-black">
-                        <p className="text-[26px]">
-                            Stock
-                        </p>
-                        <img src="stocks.gif" alt="Stocks Animation" className="w-60 h-20 ml-20 mr-20 hover:opacity-80 duration-200">
-                        </img>
-                        <p className="text-[26px]">
-                            Value
-                        </p>
-                    </div>
-
-                    <div className="h-[125px] justify-center items-center 
-                    flex border-black border-[1px] border-l-0 text-black">
-                        <p className="text-[26px]">
-                            Stock
-                        </p>
-                        <img src="stocks.gif" alt="Stocks Animation" className="w-60 h-20 ml-20 mr-20 hover:opacity-80 duration-200">
-                        </img>
-                        <p className="text-[26px]">
-                            Value
-                        </p>
-                    </div>
-
-                </div> 
-            </div>
+    <div className="flex flex-col h-screen bg-gradient-to-b from-white to-gray-100 font-sans">
+      {/* Top Section */}
+      <div className="flex flex-1 p-6 gap-6">
+        {/* Image Box */}
+        <div className="rounded-2xl overflow-hidden shadow-xl w-[550px] h-[500px]">
+          <img
+            src="stocks.gif"
+            alt="Stocks Animation"
+            className="w-full h-full object-cover"
+          />
         </div>
-        <div className="flex h-[100px] w-[1300px] justify-center items-center">
-            <button className="h-full w-[190px] text-black text-[20px]
-            hover:bg-gray-200 border-r">
-                Day
-            </button>
-            <button className="h-full w-[190px] text-black text-[20px]
-            hover:bg-gray-200 border-r">
-                Week
-            </button>
-            <button className="h-full w-[190px] text-black text-[20px]
-            hover:bg-gray-200 border-r">
-                Month
-            </button>
-            <button className="h-full w-[190px] text-black text-[20px]
-            hover:bg-gray-200 border-r">
-                Year
-            </button>
-            <button className="h-full w-[190px] text-black text-[20px]
-            hover:bg-gray-200 border-r">
-                5 Years
-            </button>
-            <div className="flex h-full w-full justify-center items-center text-white text-[32px] bg-black hover:bg-gray-800">
-                    Search Stocks
-            </div>
+
+        {/* Stock List */}
+        <div className="flex flex-col flex-1 bg-white border border-gray-300 rounded-2xl shadow-xl overflow-hidden h-[500px]">
+          <p className="text-[42px] font-semibold text-gray-800 py-4 border-b border-gray-300">
+            Top Stocks
+          </p>
+
+          <div className="flex flex-col overflow-y-auto divide-y divide-gray-200">
+            {[1, 2, 3, 4].map((item) => (
+              <div
+                key={item}
+                className="h-[120px] flex justify-between items-center px-10 hover:bg-gray-50 transition"
+              >
+                <p className="text-[26px] text-gray-700 font-medium">Stock</p>
+                <img
+                  src="stocks.gif"
+                  alt="Stocks Animation"
+                  className="w-48 h-20 opacity-90 hover:opacity-75 transition"
+                />
+                <p className="text-[26px] text-gray-700 font-medium">Value</p>
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
+
+      {/* Bottom Buttons */}
+      <div className="flex w-full h-[100px] items-center justify-center bg-white border-t">
+      <div className="flex gap-4">
+        {["Day", "Week", "Month", "Year", "5 Years"].map((label) => (
+        <button
+            key={label}
+            className="px-6 py-3 text-lg font-medium text-black rounded-xl 
+                    border border-gray-300 hover:bg-gray-100 transition">
+            {label}
+        </button>
+        ))}
+
+        <button
+        className="px-8 py-3 text-xl font-semibold text-white rounded-xl 
+                    bg-black hover:bg-gray-800 transition"
+        >
+        Search Stocks
+        </button>
+      </div>
+    </div>
     </div>
   );
-};
-
-export default Body;
+}
